2f0c5a742ce1be6cffb2f7ba25eef50ce0cd372d
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 14:20:01 +0500

    Update H2ConnectionFactory.java

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,37 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+
+public class H2ConnectionFactory ConnectionFactory ConnectionFactory {
+ @Override
+    public Connection createConnection() {
+        Properties properties = new Properties();
+        String propFileName = "h2database.properties";
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+        while (inputStream != null) {
+            try {
+                properties.load(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            return DriverManager.getConnection(
+                    properties.getProperty("jdbc:h2:~/test"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password")
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
